dfa7070cd37515a96dd070d1b7d76076a76b7e44
deepakmp444 <deepakmp444@gmail.com>
2022-07-31 16:41:09 +0530

    formate code

--- features/users/userSlice.js
+++ features/users/userSlice.js
@@ -18,23 +18,22 @@ const userSlice = createSlice({
   name: "user",
   initialState,
   extraReducers: (builder) => {
-    builder.addCase(fetchUsers.pending, state=>{
-        state.loading = true
+    builder.addCase(fetchUsers.pending, (state) => {
+      state.loading = true;
     });
 
-    builder.addCase(fetchUsers.fulfilled, (state, action)=>{
-        state.loading = false,
-        state.user = action.payload,
-        state.error = ''
+    builder.addCase(fetchUsers.fulfilled, (state, action) => {
+      (state.loading = false),
+        (state.user = action.payload),
+        (state.error = "");
     }),
-     builder.addCase(fetchUsers.rejected, (state, action)=>{
-        state.loading = false,
-        state.user = [],
-        state.error = action.error.message
-     })
+      builder.addCase(fetchUsers.rejected, (state, action) => {
+        (state.loading = false),
+          (state.user = []),
+          (state.error = action.error.message);
+      });
+  },
+});
 
-  }
-})
-
-module.exports = userSlice.reducer
+module.exports = userSlice.reducer;
 module.exports.fetchUsers = fetchUsers;
